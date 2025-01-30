683c93ebb9f51421b5d7e55883a9355f5b1affe9
paullexxus <136038271+paullexxus@users.noreply.github.com>
2025-01-30 13:38:36 +0800

    multitextinput

---
+++ Activity2-MultiTextinput/graceful-orange-turkish-delight/App.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const TextInputExample = () => {
+  const [name, onChangeTextName] = React.useState('Name: ');
+  const [age, onChangeTextAge] = React.useState('Age: ');
+  const [address, onChangeAddress] = React.useState('Address: ');
+  const [school, onChangeSchool] = React.useState('School: ');
+  const [course, onChangeCourse] = React.useState('Course: ');
+  const [email, onChangeEmail] = React.useState('Email: ');
+  const [number, onChangeContact] = React.useState('Contact Number: ');
+  const [value, onChangeText] = React.useState('About me: ');
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={[
+          styles.container,
+          {
+            backgroundColor: value,
+          },
+        ]}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeTextName}
+          value={name}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeTextAge}
+          value={age}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeAddress}
+          value={address}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeSchool}
+          value={school}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeCourse}
+          value={course}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeEmail}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeContact}
+          value={number}
+        />
+        <TextInput
+          editable
+          multiline
+          numberOfLines={4}
+          maxLength={40}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          style={styles.textInput}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  container: {
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    padding: 10,
+  },
+});
+
+export default TextInputExample;
